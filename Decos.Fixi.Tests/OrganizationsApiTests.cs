119d573b6f5f7c7915f204183f063f087a0b655f
Mahesh Bhosale <Mahesh.Bhosale@decos.com>
2020-02-20 11:35:08 +0530

    Use new lightweight endpoint for get organizations

---
+++ Decos.Fixi.Tests/OrganizationsApiTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Decos.Fixi.Tests
+{
+  [TestClass]
+  public class OrganizationsApiTests : FixiClientTest
+  {
+    private string RegionShortName => Parameter("region") ?? "decos";
+
+    [TestMethod]
+    public async Task FindReturnsResults()
+    {
+      var regions = await FixiClient.Organizations.FindAsync();
+      regions.Results.ShouldNotBeEmpty();
+    }
+
+    [TestMethod]
+    public async Task FindWithOrganizationShortNameReturnsResult()
+    {
+      var organization = Parameter("organization") ?? "decos";
+
+      var org = await FixiClient.Organizations.FindByIdAsync(organization);
+      Assert.Equals(org.ShortName, organization);
+    }
+  }
+}
