04d4e5f85f3ce11618a12b9f5459d3d7d7b7f46c
Mahesh Bhosale <Mahesh.Bhosale@decos.com>
2020-02-20 11:40:52 +0530

    Removed get organization test

--- Decos.Fixi.Tests/OrganizationsApiTests.cs
+++ Decos.Fixi.Tests/OrganizationsApiTests.cs
@@ -18,14 +18,5 @@ namespace Decos.Fixi.Tests
       var regions = await FixiClient.Organizations.FindAsync();
       regions.Results.ShouldNotBeEmpty();
     }
-
-    [TestMethod]
-    public async Task FindWithOrganizationShortNameReturnsResult()
-    {
-      var organization = Parameter("organization") ?? "decos";
-
-      var org = await FixiClient.Organizations.FindByIdAsync(organization);
-      Assert.Equals(org.ShortName, organization);
-    }
   }
 }
